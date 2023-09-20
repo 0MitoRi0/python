4a7c59fd3f0a21377531705530a175bb571c6c4b
mi_To_ri <144074689+0MitoRi0@users.noreply.github.com>
2023-09-20 12:00:56 +0700

    Add files via upload

---
+++ Python/DTIpython02.py/DTIpython02.py
@@ -0,0 +1,6 @@
+if 7 > 20**2 :
+    print('hi   ')
+    print('hey')
+    print('------------')
+
+print('SAU----------')
---
+++ Python/DTIpython02.py/DTIpython06.py
@@ -0,0 +1,19 @@
+# for
+
+for x in range(5):
+    print(f'DTI-{x}')
+
+print('..................')
+
+for y in range(2,5):
+    print(f'SAU-{y}')
+
+print('..................')
+
+for z in range(2,5,3):
+    print(f'DTI1-{z}')
+
+for a in range(3,30,5):
+    print(f'D1-{a}')
+
+print('hello..')
