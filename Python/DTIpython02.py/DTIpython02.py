9afbe5fafbb924d481c35025d35b07ce228082c3
mi_To_ri <144074689+0MitoRi0@users.noreply.github.com>
2023-09-21 17:57:09 +0700

    Delete Python directory

--- Python/DTIpython02.py/DTIpython02.py
+++
@@ -1,6 +0,0 @@
-if 7 > 20**2 :
-    print('hi   ')
-    print('hey')
-    print('------------')
-
-print('SAU----------')
--- Python/DTIpython02.py/DTIpython03.py
+++
@@ -1,9 +0,0 @@
-if 7 < 20 ** 2 :
-    print('hi   ')
-    print('hey')
-    print('------------')
-else :
-    print('SAU----------')
-    print('SAU----------')
-
-print('SAU----------')
